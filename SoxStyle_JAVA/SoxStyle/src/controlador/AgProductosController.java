0664799a1969c83af4e5a0e0a7d9b5494a82bd38
Maicol <juanjoseg569@gmail.com>
2023-07-27 14:14:42 -0500

    Agregar validacion
    Se agrego la validacion de descuento del producto y se comento cierta parte del codigo

--- SoxStyle_JAVA/SoxStyle/src/controlador/AgProductosController.java
+++ SoxStyle_JAVA/SoxStyle/src/controlador/AgProductosController.java
@@ -29,54 +29,57 @@ import controlador.*;
 
 public class AgProductosController implements ActionListener, KeyListener{
 	
+	//Se instancia la vista de agregar productos. Esta sera la parte visual de este codigo.
 	AgregarProductos ap = new AgregarProductos();
+	
+	//Se instacia la clase la cual contiene las validaciones de los campos de textos existentes.
 	Validaciones vali = new Validaciones();
+	
+	//Se instancia las consultas para que la vista mande los datos hacia la base de datos.
 	Consultas consul = new Consultas();
+	
+	//Se hace una instacia de la vista de administrador para tomar algunos "id's".
 	Administrador a;
+	
+	//Esta es una variable la cual se guarda la imagen que se subira como principal.
 	Path Origen;
+	
+	//Este es el formato el cual es una ayuda para enviar la imagen.
 	String FinFormato;
+	
+	//Este es el metodo constructor el cual recibe la vista de AgregarProductos y la de Administrador.
 	public AgProductosController (AgregarProductos ap, Administrador a) {
 		
 		this.ap = ap;
 		this.a = a;
+		
+		//Le damos el metodo de "addActionListener" a los botones para que sean escuchados y tengan funcionalidad.
 		ap.btnBAImagen.addActionListener(this);
 		ap.btnAgregar.addActionListener(this);
+		
+		//Le damos el metodo de "addKeyListener" a los campos de textos para que los errores se eliminen cuando se escriba alguna letra
 		ap.txtNombre.addKeyListener(this);
 		ap.txtPrecio.addKeyListener(this);
 		ap.txtDescuento.addKeyListener(this);
+		
+		//Se llama el metodo "box" el cual contiene las categorias. 
 		box();
 		
 	}
 	
-
-		
-//	validacion del signo $ que queda pendiente 
-	
-//	public void actionPerformed2(ActionEvent e) {
-//		if(e.getSource().equals(ap.btnAgregar)){
-//		ap.txtPrecio.addKeyListener((KeyListener) new KeyAdapter() {
-//		    public void keyTyped(KeyEvent e) {
-//		        char c = e.getKeyChar();
-//		        if (c != '$') {
-//		            e.consume(); 
-//		        }else {
-//		        	JOptionPane.showMessageDialog(null, "error");
-//		        }
-//		    }
-//		
-//		});
-//		}
-//	}
-
-	
 	public void actionPerformed(ActionEvent e) {
 		
+		//Con este codigo, le damos una funcionalidad al boton de agregar imagen.
 		if(e.getSource().equals(ap.btnBAImagen)) {
-
+			
+			//--------------------------------------
 			JFileChooser file = new JFileChooser();
 			file.showOpenDialog(null);
 			File archivo = file.getSelectedFile();
+			//--------------------------------------
 			
+			//En este "if" se valida la imagen que no este vacia y que este en el formato requeruido. Una vez este correcto
+			//se desaparece el boton de agregar imagen" y se muestra un lbl con la ruta de la misma.
 			if(archivo != null) {
 				int formato = archivo.getName().length() - 4;
 				FinFormato = archivo.getName().toString().substring(formato);
@@ -96,6 +99,10 @@ public class AgProductosController implements ActionListener, KeyListener{
 				}
 			}
 		}
+		//-------------------------------------------------------------------------------------------------------------------
+		
+		//Cuando presionemos el boton de agregar produtos. Se crea un modelo de productos y despues se guarda en variables
+		//lo que este en el campo de texto.
 		
 		if(e.getSource().equals(ap.btnAgregar)){
 			
@@ -104,6 +111,9 @@ public class AgProductosController implements ActionListener, KeyListener{
 			String preciopro = ap.txtPrecio.getText();
 			String descuentopro = ap.txtDescuento.getText();	
 			String txtArea = ap.textAreaDescripcion.getText();
+			
+		//---------------------------------------------------------------------------------------------------------------------	
+			
 			
 			//Validaciones de el campo Nombre Producto
 			
@@ -139,6 +149,29 @@ public class AgProductosController implements ActionListener, KeyListener{
 				}
 			
 			//Validaciones del campo Descuento
+			
+			else
+				if(Validaciones.vacio(descuentopro)) {
+						
+					ap.lblErrorDescuento.setText("Campo vacio");
+					ap.lblErrorDescuento.setVisible(true);
+						
+				}
+			else 
+				if(Validaciones.SoloNum(descuentopro)) {
+						
+					ap.lblErrorDescuento.setText("Ingrese Numeros");
+					ap.lblErrorDescuento.setVisible(true);
+						
+				}
+				
+			else 
+				if(Validaciones.CantidadDescuento(descuentopro)) {
+						
+					ap.lblErrorDescuento.setText("Descuento Incorrecto");
+					ap.lblErrorDescuento.setVisible(true);
+						
+				}
 
 			//Validaciones del campo de descripcion 
 			else
@@ -148,9 +181,13 @@ public class AgProductosController implements ActionListener, KeyListener{
 					ap.lblErrorDescripcion.setVisible(true);
 				
 				}
-				else if(Origen == null) {
+			
+			//Validacion de la imagen. 
+			else 
+				if(Origen == null) {
 					JOptionPane.showMessageDialog(null, "AGREGUE UNA IMAGEN");
 				}
+				
 				else {
 					
 					//Hacemos las conexiones de los campos con la base de datos
