25bfe45a688bf1d2e086b05138978b8a88f99786
Leduan <leduan12044@gmail.com>
2023-08-01 14:24:31 -0500

    comentarios agregados

--- SoxStyle_JAVA/SoxStyle/src/controlador/PaginaProductosController.java
+++ SoxStyle_JAVA/SoxStyle/src/controlador/PaginaProductosController.java
@@ -17,84 +17,83 @@ import sql.*;
 
 
 public class PaginaProductosController implements ActionListener, KeyListener {
+
+    Validaciones validar = new Validaciones(); // Objeto_para_realizar_validaciones
+    DefaultTableModel modelo = new DefaultTableModel(); // Modelo_de_datos_para_la_tabla
+
+    Consultas consultar = new Consultas(); // Objeto_para_realizar_consultas_a_la_base_de_datos
+    PaginaProductos vista; // Referencia_a_la_vista_"PaginaProductos"
+    EditarProductos editarPanel; // Referencia_a_la_vista_"EditarProductos"
+    EdProductosController editar; // Controlador_para_la_vista_"EditarProductos"
+    Administrador a; // Referencia_a_la_vista_"Administrador"
+    Productos pd = new Productos(); // Objeto_para_manejar_datos_de_productos
+
+    // Constructor_que_recibe_las_vistas_"PaginaProductos"_y_"Administrador"
+    public PaginaProductosController(PaginaProductos vista, Administrador a) {
+        this.vista = vista;
+        this.a = a;
+        // Agrega_el_controlador_como_oyente_a_los_botones_de_la_vista_"PaginaProductos"
+        this.vista.btnAgregarProductos.addActionListener(this);
+        this.vista.btnEditar.addActionListener(this);
+        this.vista.btnEliminar.addActionListener(this);
+        this.vista.btnCancelar.addActionListener(this);
+        // Llama_a_diferentes_métodos_para_configurar_y_cargar_datos_en_la_vista
+        eventos();
+        buscadorr();
+        datostabla();
+    }
+
+
 	
-	    Validaciones validar = new Validaciones(); // Objeto para realizar validaciones
-	    DefaultTableModel modelo = new DefaultTableModel(); // Modelo de datos para la tabla
-	  
-	    Consultas consultar = new Consultas(); // Objeto para realizar consultas a la base de datos
-	    PaginaProductos vista; // Referencia a la vista "PaginaProductos"
-	    EditarProductos editarPanel; // Referencia a la vista "EditarProductos"
-	    EdProductosController editar; // Controlador para la vista "EditarProductos"
-	    Administrador a; // Referencia a la vista "Administrador"
-	    Productos pd = new Productos(); // Objeto para manejar datos de productos
-	    
-	    // Constructor que recibe las vistas "PaginaProductos" y "Administrador"    
-	public PaginaProductosController(PaginaProductos vista, Administrador a) {
-		this.vista = vista;
-		this.a = a;
-		// Agrega el controlador como oyente a los botones de la vista "PaginaProductos"
-		this.vista.btnAgregarProductos.addActionListener(this);
-		this.vista.btnEditar.addActionListener(this);
-		this.vista.btnEliminar.addActionListener(this);
-		this.vista.btnCancelar.addActionListener(this);
-	    // Llama a diferentes métodos para configurar y cargar datos en la vista
-		eventos();
-		buscadorr();
-		datostabla();
-		
-	}
 	
-		public void actionPerformed2(ActionEvent e) {
-			if(e.getSource().equals(vista.btnAgregarProductos)){
-			 JOptionPane.showMessageDialog(null, "ir a vista de agregar productos");
-				AgregarProductos ag = new AgregarProductos();
-				 new AgProductosController(ag, a);
-				   Ayudas.ActualizarPanel(ag, vista);
-					
-					 }
-		         }
 			
 		
 		 
 		   
 		   
-		   public void eventos() {
-				
-			   vista.tblProductos.addMouseListener(new MouseAdapter() {
+	
+		// Método_para_configurar_los_eventos_asociados_a_la_tabla_de_productos.
+		// Cuando_se_seleccione_una_fila_en_la_tabla,_se_mostrarán_ciertos_botones_y_se_obtendrán_los_valores_de_la_fila_seleccionada.
+		public void eventos() {
+		    // Añadir_un_adaptador_de_eventos_del_mouse_a_la_tabla_de_productos
+		    vista.tblProductos.addMouseListener(new MouseAdapter() {
 
-					@Override
-					public void mouseReleased(MouseEvent e) {
-						if(e.getSource().equals(vista.tblProductos)) {
-							vista.btnEditar.setVisible(true);
-							vista.btnEliminar.setVisible(true);
-							vista.btnCancelar.setVisible(true);
-						}
-			               
-			             
-			                    // Obtener los valores de la fila seleccionada en la tabla
-								int filaSeleccionada = vista.tblProductos.getSelectedRow();
-								pd.setId((Integer.parseInt(vista.tblProductos.getValueAt(filaSeleccionada, 0).toString())));
-			                    String nombre = (String) vista.tblProductos.getValueAt(filaSeleccionada, 1).toString();
-			                    pd.setNombre(nombre);
-			                    String precio = (String)vista.tblProductos.getValueAt(filaSeleccionada, 2).toString();
-			                    pd.setPrecio(Integer.parseInt(precio));
-			                    String descuento = (String)vista.tblProductos.getValueAt(filaSeleccionada, 3).toString();
-			                    pd.setDescuento(Integer.parseInt(descuento));
-			                    String descripcion =(String) vista.tblProductos.getValueAt(filaSeleccionada, 4).toString();
-			                    pd.setDescripcion(descripcion);
-			                    
-			                    String estado =(String) vista.tblProductos.getValueAt(filaSeleccionada, 9).toString();
-			                    pd.setEstado(String.valueOf(estado));
-			                 
-			                  
+		        @Override
+		        public void mouseReleased(MouseEvent e) {
+		            // Verificar_si_la_fuente_del_evento_es_la_tabla_de_productos
+		            if (e.getSource().equals(vista.tblProductos)) {
+		                // Mostrar_los_botones_de_edición,_eliminación_y_cancelación
+		                vista.btnEditar.setVisible(true);
+		                vista.btnEliminar.setVisible(true);
+		                vista.btnCancelar.setVisible(true);
+		            }
 
-			             
-			                
-					}
-				});
-			
+		            // Obtener_el_índice_de_la_fila_seleccionada_en_la_tabla_de_productos
+		            int filaSeleccionada = vista.tblProductos.getSelectedRow();
+		            // Obtener_el_ID_del_producto_de_la_fila_seleccionada_y_almacenarlo_en_el_objeto_pd
+		            pd.setId((Integer.parseInt(vista.tblProductos.getValueAt(filaSeleccionada, 0).toString())));
+		            // Obtener_el_nombre_del_producto_de_la_fila_seleccionada_y_almacenarlo_en_el_objeto_pd
+		            String nombre = (String) vista.tblProductos.getValueAt(filaSeleccionada, 1).toString();
+		            pd.setNombre(nombre);
+		            // Obtener_el_precio_del_producto_de_la_fila_seleccionada_y_almacenarlo_en_el_objeto_pd
+		            String precio = (String) vista.tblProductos.getValueAt(filaSeleccionada, 2).toString();
+		            pd.setPrecio(Integer.parseInt(precio));
+		            // Obtener_el_descuento_del_producto_de_la_fila_seleccionada_y_almacenarlo_en_el_objeto_pd
+		            String descuento = (String) vista.tblProductos.getValueAt(filaSeleccionada, 3).toString();
+		            pd.setDescuento(Integer.parseInt(descuento));
+		            // Obtener_la_descripción_del_producto_de_la_fila_seleccionada_y_almacenarla_en_el_objeto_pd
+		            String descripcion = (String) vista.tblProductos.getValueAt(filaSeleccionada, 4).toString();
+		            pd.setDescripcion(descripcion);
+		            // Obtener_el_estado_del_producto_de_la_fila_seleccionada_y_almacenarlo_en_el_objeto_pd
+		            String estado = (String) vista.tblProductos.getValueAt(filaSeleccionada, 9).toString();
+		            pd.setEstado(String.valueOf(estado));
+		        }
+		    });
 		}
-		// Método para manejar la acción del botón "btnAgregarProductos"  
+
+
+
+		// Método_para_manejar_la_acción_del_botón "btnAgregarProductos"  
 		   public void actionPerformed(ActionEvent e) {
 		
 				
@@ -103,11 +102,11 @@ public class PaginaProductosController implements ActionListener, KeyListener {
 						
 						//esto_es_una_ayuda_para_mandar_los_datos_de_un_jpanel
 						//a_otro
-					  	 new EdProductosController(ed, pd, a); // Crea controlador para la vista "EditarProductos"
-				         Ayudas.ActualizarPanel(ed, a.panelPrincipal); // Actualiza el panel principal con la vista "EditarProductos"
+					  	 new EdProductosController(ed, pd, a); // Crea_controlador_para_la vista "EditarProductos"
+				         Ayudas.ActualizarPanel(ed, a.panelPrincipal); // Actualiza_el panel principal con_la vista "EditarProductos"
 					}
 		   else {
-			// Desactiva el producto seleccionado de la base de datos
+			// Desactiva_el producto_seleccionado_de la_base de_datos
 				if(e.getSource().equals(vista.btnEliminar)) {
 					
 						consultar.eliminarProducto(pd);
@@ -131,7 +130,7 @@ public class PaginaProductosController implements ActionListener, KeyListener {
 					}
 				}
 			}
-					datostabla(); // Actualiza los datos de la tabla de productos
+					datostabla(); // Actualiza_los datos de la tabla de productos
 		   }
 
 
